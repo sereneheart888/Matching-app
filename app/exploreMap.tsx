a2cf041f34663ab33aef312085f70b2d03dd7a24
Ellio <sereneheart888@gmail.com>
2024-08-29 04:07:11 +0300

    [U] pin style

--- app/exploreMap.tsx
+++ app/exploreMap.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Callout } from 'react-native-maps';
 import { Marker } from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function ExploreMap() {
     const [region, setRegion] = useState(getInitialState().region);
@@ -17,16 +17,89 @@ export default function ExploreMap() {
         };
     }
 
+    const HERE_API_KEY = 'AIzaSyCUe0kufF120t7f4IVJRNvIcDjhkKlE6dY';
+
     function onRegionChangeComplete(region: any) {
-        console.log(region);
         setRegion(region);
     }
+
+    const userData = [
+        { name: 'Yamada Yuki', age: 25, hobby: 'Music, Book...', image: <Image source={require('../assets/images/user.png')} style={styles.icon} /> },
+        { name: 'Nasuyi Akiko', age: 22, hobby: 'TV, Resturant...', image: <Image source={require('../assets/images/user1.png')} style={styles.icon} /> },
+        { name: 'Tomoda Hayaki', age: 23, hobby: 'Swimming, Game...', image: <Image source={require('../assets/images/user2.png')} style={styles.icon} /> },
+        { name: 'Maya', age: 27, hobby: 'Dance, Yard...', image: <Image source={require('../assets/images/user3.png')} style={styles.icon} /> }]
+
+    const [user, setUser] = useState(userData[0])
+    // function getAddressFromCoordinates(region: any) {
+    //     return new Promise((resolve, reject) => {
+    //         fetch(
+    //             'https://maps.googleapis.com/maps/api/geocode/json?address=' +
+    //             region.latitude +
+    //             ',' +
+    //             region.longitude +
+    //             '&key=' +
+    //             HERE_API_KEY,
+    //         )
+    //             .then(response => response.json())
+    //             .then(responseJson => {
+    //                 console.log(responseJson);
+    //                 if (responseJson.status === 'OK') {
+    //                     resolve(responseJson?.results?.[0]?.formatted_address);
+    //                 } else {
+    //                     console.log("not found")
+    //                     reject('not found');
+    //                 }
+    //             })
+    //             .catch(error => {
+    //                 reject(error);
+    //             });
+    //     });
+    // }
+
+    useEffect(() => {
+        // getAddressFromCoordinates(region)
+        const userNumber = Math.round(Math.random() * 3);
+        console.log(userNumber)
+        setUser(userData[userNumber])
+    }, [region])
+
     return (
-        <View style={styles.container} >
-            <MapView style={styles.map} region={region} onRegionChangeComplete={onRegionChangeComplete} onPress={(e) => setMarkers(e.nativeEvent.coordinate)}>
-                {
-                    <Marker coordinate={markers} />
-                }
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                region={region}
+                onRegionChangeComplete={onRegionChangeComplete}
+                onPress={(e) => setMarkers(e.nativeEvent.coordinate)}
+                showsUserLocation={true}
+            >
+                <Marker coordinate={markers} image={require('../assets/images/pin.png')}>
+                    <Callout style={{ height: 70, width: 200 }} tooltip>
+                        <View
+                            style={{
+                                flex: 2,
+                                backgroundColor: '#ffff54',
+                                borderRadius: 50,
+                                flexDirection: 'row',
+                            }}
+                        >
+                            <Text style={styles.iconPadding}>
+                                {/* <Image source={require('../assets/images/user.png')} style={styles.icon} /> */}
+                                {user.image}
+                            </Text>
+                            <View
+                                style={{
+                                    flexDirection: 'column',
+                                    paddingTop: 5,
+                                }}
+                            >
+                                <Text style={{ color: '#5c5c5c', fontWeight: '700' }}>{user.name}</Text>
+                                <Text style={{ color: '#5c5c5c', fontWeight: '700' }}>{user.age}</Text>
+                                <Text style={{ color: '#5c5c5c' }}>{user.hobby}</Text>
+                            </View>
+                            {/* </View> */}
+                        </View>
+                    </Callout>
+                </Marker>
             </MapView>
         </View>
     );
@@ -40,4 +113,15 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
     },
+    icon: {
+        width: 50,
+        height: 50,
+        resizeMode: 'contain'
+    },
+    iconPadding: {
+        width: '40%',
+        height: '100%',
+        marginTop: -10,
+        paddingLeft: 20,
+    }
 });
